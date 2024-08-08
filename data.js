2beabb2e53e0d29d413011f0a00271990a0bbbdc
geoseo360 <geoseo360@gmail.com>
2024-08-08 14:38:05 +0400

    commit

---
+++ data.js
@@ -0,0 +1,315 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-berghoff-store",
+      "name": "BergHOFF store",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.008857551197371194,
+        "pitch": 0.16948250384894337,
+        "fov": 1.4650093334787428
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1492807490049994,
+          "pitch": 0.2057882754578113,
+          "rotation": 0,
+          "target": "1-berghoff"
+        },
+        {
+          "yaw": 2.5858470492734247,
+          "pitch": 0.05772125246663862,
+          "rotation": 0,
+          "target": "5-3rd-floor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.41677935667899035,
+          "pitch": 0.6073123861076262,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6572067961518133,
+          "pitch": 0.2528697696540405,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.8437401259444695,
+          "pitch": 0.22529266812266613,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-berghoff",
+      "name": "BergHOFF",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.08923043789901897,
+        "pitch": 0.08530735777527809,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0723237100105916,
+          "pitch": 0.15865120488645346,
+          "rotation": 0,
+          "target": "5-3rd-floor"
+        },
+        {
+          "yaw": -1.7078621510271308,
+          "pitch": 0.19567595660826242,
+          "rotation": 0,
+          "target": "0-berghoff-store"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.20024598106699187,
+          "pitch": 0.2873913391160734,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-mok-coffee-1",
+      "name": "MOK Coffee 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.1593537349654177,
+        "pitch": 0.19677886126804367,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.906980380285173,
+          "pitch": 0.29526290183633463,
+          "rotation": 0,
+          "target": "4-mok-coffee"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-mok-coffee-guest-area",
+      "name": "MOK Coffee guest area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.9535424941347657,
+        "pitch": 0.1667537457493129,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1568382263436376,
+          "pitch": 0.10303245115438031,
+          "rotation": 0,
+          "target": "4-mok-coffee"
+        },
+        {
+          "yaw": 2.330111301892151,
+          "pitch": 0.06997249239702086,
+          "rotation": 0,
+          "target": "5-3rd-floor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.936184868454827,
+          "pitch": 0.5257026397894062,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-mok-coffee",
+      "name": "MOK Coffee",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.05623838700870465,
+        "pitch": 0.054286500402458415,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.749071878054604,
+          "pitch": 0.020316138870747125,
+          "rotation": 0,
+          "target": "3-mok-coffee-guest-area"
+        },
+        {
+          "yaw": 2.48203681140141,
+          "pitch": 0.09164352471274739,
+          "rotation": 0,
+          "target": "2-mok-coffee-1"
+        },
+        {
+          "yaw": -1.0041995282563736,
+          "pitch": 0.17496785373487178,
+          "rotation": 0,
+          "target": "5-3rd-floor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5896895631497845,
+          "pitch": 0.38600329098458985,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-3rd-floor",
+      "name": "3rd floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.06787391535533338,
+        "pitch": 0.03619100026829969,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6667377148182716,
+          "pitch": 0.0642680350095457,
+          "rotation": 0,
+          "target": "4-mok-coffee"
+        },
+        {
+          "yaw": 1.494533229832518,
+          "pitch": 0.048005942188286355,
+          "rotation": 0,
+          "target": "1-berghoff"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
